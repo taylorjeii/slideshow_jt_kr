dac107cd506e6ad9cef668ee1f76ef4e39b3b31a
jtaylor1991 <james.taylorii91@gmail.com>
2016-08-03 21:31:42 -0400

    removed conflicts

--- slideshow.js
+++ slideshow.js
@@ -2,20 +2,12 @@ var slideshow = {
 	photoList: ['first', 'second', 'third', 'fourth', 'fifth'],
 	currentPhotoIndex: 0,
 	nextPhoto: function () {
-<<<<<<< HEAD
-		if (this.currentPhotoIndex > this.photoList.length - 1){
-=======
 		this.currentPhotoIndex++;
 		if (this.currentPhotoIndex === this.photoList.length ){
->>>>>>> 9fb931c3f550b4e1b10fff0071a3d2ffa59ef607
 			console.log('end of slideshow');
 			this.currentPhotoIndex--;
 		} else {
 			this.getCurrentPhoto();
-<<<<<<< HEAD
-			this.currentPhotoIndex += 1;
-=======
->>>>>>> 9fb931c3f550b4e1b10fff0071a3d2ffa59ef607
 		}
 	},
 	prevPhoto: function () {
@@ -25,24 +17,16 @@ var slideshow = {
 			this.currentPhotoIndex++;
 		} else {
 			this.getCurrentPhoto();
-<<<<<<< HEAD
-			this.currentPhotoIndex -= 1;
-=======
->>>>>>> 9fb931c3f550b4e1b10fff0071a3d2ffa59ef607
 		}
 	},
 	getCurrentPhoto: function () {
-		console.log(this.photoList[this.currentPhotoIndex] + ': index: ' + this.currentPhotoIndex);
+		console.log(this.photoList[this.currentPhotoIndex]);
 	}
 }
 
-<<<<<<< HEAD
-slideshow.nextPhoto(); 
-=======
 slideshow.getCurrentPhoto();
 
 slideshow.nextPhoto();
->>>>>>> 9fb931c3f550b4e1b10fff0071a3d2ffa59ef607
 slideshow.nextPhoto();
 slideshow.nextPhoto();
 slideshow.nextPhoto();
